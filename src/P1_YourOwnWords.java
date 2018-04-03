796d9e7a6d9389547e9ef79b11d22a9077593c1d
drek226 <37354452+drek226@users.noreply.github.com>
2018-04-03 14:08:50 -0500

    p1&2 finished

--- src/P1_YourOwnWords.java
+++ src/P1_YourOwnWords.java
@@ -17,34 +17,40 @@ x < y
 
 /* 1. 
 -- Your conditional expression
+y >= 1
 
 
 -- Your test values for x, y, z and do you expect true or false?
-
+1 or > is true
+0 =< is false
 */
 
 /* 2. 
 -- Your conditional expression
-
+x is not zero
+x <0
+x >0 
 
 -- Your test values for x, y, z and do you expect true or false?
-
+any number not 0 will return true / a false means the number is 0 
 */
 
 /* 3.  
 -- Your conditional expression
-
+y * z is odd
+(y * x)  % 2 == 0
 
 -- Your test values for x, y, z and do you expect true or false?
-
+if the number is even then the you get a false return 
 */
 
 /* 4.  
 -- Your conditional expression
-
+y is at least the sum of z and x
+y>= z + x
 
 -- Your test values for x, y, z and do you expect true or false?
-
+True // 1,2,1
 */
 
 public class P1_YourOwnWords {
@@ -59,7 +65,7 @@ public class P1_YourOwnWords {
 		
 		// Replace the conditional test, x < y below with your conditional expression to verify it is correct
 		// using your test values
-		if( x < y ) {
+		if(y >=(z + x)) {
 			System.out.println("The test was TRUE");
 		} else {
 			System.out.println("The test was FALSE");
